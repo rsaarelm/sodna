b735d77fcdde3f6643f9ee2fbd29c83536ab076e
Risto Saarelma <risto.saarelma@iki.fi>
2015-05-19 15:24:53 +0300

    Update font bake tool

--- tools/bake_font.sh
+++ tools/bake_font.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-# Convert a font bitmap into a C include file with 8-bit grayscale data.
-# Requires ImageMagick and xxd.
+# Convert a 16x16 row-major font bitmap into a C include file with 8-bit
+# grayscale data. Requires expr, ImageMagick and xxd.
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: `basename $0` infile.png"
     exit 1
 fi
 
+if !command -v xxd &> /dev/null; then
+    echo "xxd not installed"
+    exit 1
+fi
+
+if !command -v identify &> /dev/null; then
+    echo "ImageMagick not installed"
+    exit 1
+fi
+
 convert $1 $1.gray || exit 1
-echo "const int default_font_w = `identify -format '%w' $1` / 16;"
-echo "const int default_font_h = `identify -format '%h' $1` / 16;"
-echo "const uint8_t default_font[] = {"
+w=`identify -format '%w' $1`
+h=`identify -format '%h' $1`
+echo "{ `expr $w / 16`, `expr $h / 16`, `identify -format '%w' $1`,"
 cat $1.gray | xxd -i
-echo "};"
+echo "}"
 
 rm $1.gray
